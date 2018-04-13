f9b1adf69aa8a3da21d1695bc976ed948975acf5
juanjo <juanjo@juanjo-VirtualBox>
2018-04-13 16:20:42 +0200

    Versión 1.1 Añadir el mensaje "Hola Mundo

--- holahola/src/holahola/Holahola.java
+++ holahola/src/holahola/Holahola.java
@@ -17,6 +17,7 @@ public class Holahola {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola,mundo");
+        System.out.println("----------------");
     }
     
 }
